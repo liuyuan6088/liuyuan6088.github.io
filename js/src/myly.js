a832c141fac77eebb6096607d1a7de4f8bd2814e
liuyuan <939692178@qq.com>
2018-05-13 00:25:07 +0800

    Site updated: 2018-05-13 00:25:05

---
+++ js/src/myly.js
@@ -0,0 +1,13 @@
+
+
+window.onload = () => {
+  document.querySelector('#btnly').onmousemove = (e) => {
+    console.log(111)
+    const x = e.pageX - e.target.offsetLeft
+    const y = e.pageY - e.target.offsetTop
+
+    e.target.style.setProperty('--x', `${x}px`)
+    e.target.style.setProperty('--y', `${y}px`)
+
+  }
+}
